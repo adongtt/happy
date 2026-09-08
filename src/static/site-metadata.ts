a8ee383f35bf1aaa89cf4b8e18edad0b9ca08e6d
adongtt <240732222@qq.com>
2026-09-08 16:26:28 +0800

    Update site metadata for new site details

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,8 +16,8 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Workouts Map',
-  siteUrl: 'https://ben29.xyz',
+  siteTitle: "adong's Workouts Map",
+  siteUrl: 'https://yizhipao.app/happy',
   logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
